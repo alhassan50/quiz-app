d89dd46b7ec821d9707dd10c91f40df76d67ef94
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-06-06 07:59:26 +0000

    Create type for category

--- src/lib/getCategory.ts
+++ src/lib/getCategory.ts
@@ -1,5 +1,11 @@
 import categories from '../../public/data/categories.json'
 
+type Category = {
+    title: string,
+    icon: string,
+    color: string,
+}
+
 export default function getCategory(categoryTitle: string) {
-    return categories.find(category => categoryTitle.toLowerCase() ===  ('/'+category.title).toLowerCase())
+    return categories.find((category: Category) => categoryTitle.toLowerCase() ===  ('/'+category.title).toLowerCase())
 }
